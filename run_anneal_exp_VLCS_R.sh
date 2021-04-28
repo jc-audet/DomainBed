b5308283c54e3fb6506f7b4d7b867fede393d902
Remus Mocanu <remus_c.m@hotmail.com>
2021-04-28 10:18:47 -0400

    back to MILA

--- run_anneal_exp_VLCS_R.sh
+++ run_anneal_exp_VLCS_R.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-#SBATCH --exclude=cn-b004,cn-c037,cn-b001,cn-a009,cn-c011
 #SBATCH --job-name=Anneal_sweep_VLCS_R
 #SBATCH --output=Anneal_sweep_VLCS_R.out
 #SBATCH --error=Anneal_sweep_error_VLCS_R.out
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:4
-#SBATCH --time=4-00:00:00
+#SBATCH --cpus-per-task=6
+#SBATCH --gres=gpu:8
+#SBATCH --time=3-00:00:00
 #SBATCH --mem=100Gb
 
 # Load Modules and environements
@@ -18,40 +17,13 @@ pip3 install tqdm
 
 cd $HOME/GitRepos/DomainBed/
 
-python3 -m domainbed.scripts.train\
-       --data_dir $HOME/scratch/data/\
-       --output_dir $HOME/scratch/anneal_experiment/results/VLCS_results_ERM/1/\
-       --algorithm ERM \
-       --dataset VLCS \
-       --steps 400 \
-       --seed 1 \
-       --trial_seed 1
-       
-python3 -m domainbed.scripts.train\
-       --data_dir $HOME/scratch/data/\
-       --output_dir $HOME/scratch/anneal_experiment/results/VLCS_results_ERM/2/\
-       --algorithm ERM \
-       --dataset VLCS \
-       --steps 400 \
-       --seed 2 \
-       --trial_seed 2
-       
-python3 -m domainbed.scripts.train\
-       --data_dir $HOME/scratch/data/\
-       --output_dir $HOME/scratch/anneal_experiment/results/VLCS_results_ERM/3/\
-       --algorithm ERM \
-       --dataset VLCS \
-       --steps 400 \
-       --seed 3 \
-       --trial_seed 3
-
 python3 -m domainbed.scripts.anneal_sweep launch\
-       --algorithm IGA IRM VREx \
+       --algorithm IRM VREx \
        --dataset VLCS \
        --data_dir $HOME/scratch/data/ \
        --output_dir $HOME/scratch/anneal_experiment/results/VLCS_results_R/ \
        --command_launcher multi_gpu \
        --skip_confirmation \
-       --steps 400 \
-       --n_trials 3 \
+       --steps 300 \
+       --n_trials 2 \
        --n_anneal 20
